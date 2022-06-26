2a48114eee8545d2cc02172f712891a439b09366
Elton Koh <eltonkohjj@gmail.com>
2022-06-26 23:18:02 +0800

    initial commit

---
+++ dist/apps.js
@@ -0,0 +1,16 @@
+var arr = [ 
+    "Hougang Capeview",
+    "Kallang Wave Mall",
+    "Macpherson",
+    "Soi 19",
+    "Ci Yuan CC",
+    "Hougang Mall",
+    "Hougang Green",
+    "Samwich",
+    "Breadtalk HQ",
+    "Lor Ah Soo",
+    "Singpost",
+    "PLQ"
+];
+
+document.getElementById("values").innerHTML = arr.join('\r\n');
